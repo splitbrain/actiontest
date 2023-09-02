3ac8e65faafc24ffe77db721f98495e5f2b21092
Andreas Gohr <andi@splitbrain.org>
2023-09-02 17:30:29 +0200

    added somethign to fix

---
+++ helper.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * DokuWiki Plugin actiontest (Helper Component)
+ *
+ * @license GPL 2 http://www.gnu.org/licenses/gpl-2.0.html
+ * @author  Andreas Gohr <gohr@cosmocode.de>
+ */
+class helper_plugin_actiontest extends \dokuwiki\Extension\Plugin
+{
+
+}
+
